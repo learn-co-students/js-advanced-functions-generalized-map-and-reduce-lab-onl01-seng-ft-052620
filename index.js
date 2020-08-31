cc742fe9e4a8be352f4cb4aa313a601a9c975fae
dwandrew <danwandrew@gmail.com>
2020-08-31 19:59:55 +0100

    Done.

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function map(array, method){
 
 
 function reduce(array, method, start){
-    let total = (!!start) ? start : array[0]
-    let i = (!!start) ? 0 : 1
+    let total = !!start ? start : array[0]
+    let i = !!start ? 0 : 1
   
     for (; i < array.length; i++) {
       total = method(array[i], total)
