979f01a27a42425881f212531efdec26fc2dfbbb
shazy89 <erdo.shazy123@gmail.com>
2020-09-05 17:50:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+array = [1, 2, 3, -9]
+function map(array, someFun) {
+    let r = []
+  
+    for (let i = 0; i < array.length; i++) {
+      let theElement = array[i]
+      r.push(someFun(theElement))
+    }
+  
+    return r;
+  }
+  
+  function reduce(array, someFun, starting){
+    let r = (!!starting) ? starting : array[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < array.length; i++) {
+      r = someFun(array[i], r)
+    }
+  
+    return r;
+  }
