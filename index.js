8104e1ec0b58b27a9cf16dbb0cce5613888396fe
jessegan <jesse13579@gmail.com>
2020-09-07 12:11:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+
+function map(arr, action){
+    const res = []
+    for (let i in arr){
+        res[i] = action(arr[i])
+    }
+
+    return res
+}
+
+function reduce(arr, action,start){
+    let res = start
+
+    if (!res){
+        res = arr[0]
+
+        for (let i = 1; i < arr.length; i++){
+            res  = action(arr[i],res)
+        }
+    } else {
+        for (let i = 0; i < arr.length; i++){
+            res  = action(arr[i],res)
+        }
+    }
+
+    return res
+}
