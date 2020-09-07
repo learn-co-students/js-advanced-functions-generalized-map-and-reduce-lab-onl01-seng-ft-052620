c0e343a1a00bf3ac655e1ff4b4f3f4bd0d9dd0dc
barrettanderson <barrettanderson1989@gmail.com>
2020-09-07 10:54:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions here
+
+function map(array, method) {
+    let r = []
+
+    for (let i = 0; i < array.length; i++) {
+        r.push(method(array[i]))
+    }
+    return r
+}
