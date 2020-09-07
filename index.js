d888722a5b3640525e37c15c9baeb3825c530f0d
KaelinAlexander <kaelin.alexander@gmail.com>
2020-09-07 14:54:59 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+
+function map(sourceArray, sourceFunction) {
+    const newArray = []
+    sourceArray.forEach(element => {
+        let newValue = sourceFunction(element)
+        newArray.push(newValue)
+    })
+    return newArray
+}
+
+function reduce(sourceArray, sourceFunction, startingValue) {
+    let newTotal
+    if (startingValue) {
+        newTotal = startingValue
+    }
+    else 
+    { newTotal = 0}
+    sourceArray.forEach(element => {
+        newTotal = sourceFunction(element)
+    })
+    return newTotal
+}
