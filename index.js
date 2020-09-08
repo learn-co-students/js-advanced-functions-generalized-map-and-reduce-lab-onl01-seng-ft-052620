88f6a74ae71cbdabc0d7b5cd412bc66a780288e7
SWFullPotential <swfullpotential@gmail.com>
2020-09-08 13:36:33 -0600

    Done.

--- index.js
+++ index.js
@@ -9,8 +9,18 @@
 function map(srcArray, funct){
     let newArray = []
 
-    for (let i = 0; i<srcArray.length; i++){
-        newArray.push(funct(srcArray[i])
+    for (let i = 0; i < srcArray.length; i++){
+        newArray.push(funct(srcArray[i]))
     }
     return newArray
+}
+
+function reduce(srcArray, funct, startingPoint=0){
+    let tote = !!startingPoint ? startingPoint : srcArray[0]
+    let i = !!startingPoint ? 0 : 1
+
+    for (; i < srcArray.length; i++) {
+        tote = funct(srcArray[i], tote)
+    }
+    return tote;
 }
