dfe66dd3f78c5c732d4f3a54fdf4b9a77c8a30b7
SWFullPotential <swfullpotential@gmail.com>
2020-09-08 12:32:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Add your functions here
+function map(srcArray, funct){
+    return srcArray.map(funct)
+}
+
+function reduce(srcArray, funct, startingPoint=0){
+    return srcArray.reduce(funct, startingPoint)
+    
+}
