2a26331252a896e68617ccbe396472ead85f2f72
francepants <franceline90@gmail.com>
2020-09-08 09:16:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+// my own map-like methods
+
+function map(array, func) {
+    let map1 = []
+
+    for (let i = 0; i < array.length; i++) {
+        map1.push(func(array[i]))
+    }
+    return map1
+}
+
+// reduce
+function reduce(array, func, start){
+    let total = !!start ? start : array[0]
+    
+    let i = !!start ? 0 : 1
+  
+    for (; i < array.length; i++) {
+      total = func(array[i], total)
+    }
+  
+    return total;
+  }
