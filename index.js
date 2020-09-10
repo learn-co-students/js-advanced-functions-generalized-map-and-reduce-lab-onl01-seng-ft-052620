c0533dbf66f25233b6ca3b6961ae96a9e3de4d6e
Luca <lucakanyo@protonmail.com>
2020-09-10 14:50:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+// function map(arr, func){
+//     return arr.map(i => func(i))
+// }
+
+
+// function reduce(arr, func){
+//     return arr.reduce((item, start) => func(item, start))
+
+// }
+
 function map(arr, func){
-    return arr.map(i => func(i))
+  let result = []
+  for (let i = 0; i < arr.length; i++){
+   result.push(func(arr[i]))
+  }
+  return result
 }
 
 
-
 function reduce(arr, func, start){
-    return arr.reduce((item, start) => func(item, start))
+  let result = !!start ? start : arr[0]
+  let index = !!start ? 0 : 1
 
+  for (index; index < arr.length; index++){
+    result = func(arr[index], result)
+  }
+
+  return result
 }
 
