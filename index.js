3d39a66cea6a0ee103ceb2a8168620eacf8e5533
Luca <lucakanyo@protonmail.com>
2020-09-10 14:34:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+function map(arr, func){
+    return arr.map(i => func(i))
+}
+
+
+
+function reduce(arr, func, start){
+    return arr.reduce((item, start) => func(item, start))
+
+}
+
