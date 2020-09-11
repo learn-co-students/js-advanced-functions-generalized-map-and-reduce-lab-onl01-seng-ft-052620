3733ac9582c09a1326ed54e3195c5aa8c06f479f
Maloochow <luyao.mly@gmail.com>
2020-09-11 16:10:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Add your functions here
+function map(array, fn) {
+    return array.map(e => fn(e))
+}
+
+function reduce(array, fn, startingPoint) {
+    return startingPoint ? array.reduce(fn, startingPoint) : array.reduce(fn)
+}
