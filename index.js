fec1a81fed5cfc8e49742168d1bd35736f641f1e
ejguzman3988 <eriberto.guzman@outlook.com>
2020-09-14 02:19:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+
+function map(src, f){
+    const newArray = []
+    for (let i = 0; i<src.length; i++){
+        newArray.push(f(src[i]))
+    }
+    return newArray
+}
+
+function reduce(src, f, start = 0){
+    let total
+    let index
+    if (!!start){
+        total = start;
+        index = 0
+    }
+    else{
+        total = src[0]
+        index = 1
+    }
+
+    for (index; index<src.length; index++){
+        total = f(src[index], total)
+    }
+
+    return total
+}
+
+
