fa78b23c763c2127472d098c262a18b92417957b
chibuisi <enyia21@gmail.com>
2020-09-28 13:49:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+
+function map(userArray, userFunction){
+    let mappedArray = []
+    for(const element of userArray){
+         mappedArray.push(userFunction(element));
+    }
+    return mappedArray;
+}
+
+function reduce(userArray, userFunction){
+    let total = 0
+    for(const element of userArray){
+        total += userFunction(element)
+    }
+    return total
+}
