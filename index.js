c889fa17cebf2cde6930c0d3c45aa8df5c1ff2c6
chibuisi <enyia21@gmail.com>
2020-09-28 14:27:00 -0500

    Done.

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ function map(userArray, userFunction){
     return mappedArray;
 }
 
-function reduce(userArray, userFunction){
-    let total = 0
-    for(const element of userArray){
-        total += userFunction(element)
+function reduce(userArray, userFunction, startingPoint){
+    let total;
+    let i;
+    
+    if (startingPoint){
+        total = startingPoint
+        i = 0
+    }else {
+        total = userArray[0]
+        i = 1
+    }
+    for (i; i<userArray.length; i++) {
+        total = userFunction(userArray[i], total);
     }
     return total
 }
